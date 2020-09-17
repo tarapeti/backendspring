3c8a3a651132341e4539ab5db95c6609ad60e7b0
Taraszovics Péter <gtarapeti@gmail.com>
2020-09-17 13:23:30 +0200

    added cors config and security config with endpoint antmatchers

---
+++ src/main/java/hu/backend/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package hu.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/authentication").permitAll()
+                .antMatchers(HttpMethod.GET, "/user").permitAll()
+                .antMatchers(HttpMethod.POST, "/user").permitAll()
+                .antMatchers(HttpMethod.GET,"/user/*").permitAll()
+                .antMatchers(HttpMethod.DELETE,"/user/*").permitAll()
+                .antMatchers("/*").authenticated()
+                .anyRequest().denyAll();
+        http.cors();
+    }
+}
+
